2b9234a68366f494b82191d67fdcbadb5062adb0
GitHub Action <action@github.com>
2023-05-03 05:38:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/wonsuc/awintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wonsuc/awintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wonsuc.awintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wonsuc.awintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
